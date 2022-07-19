d2b0c1fc6c47e5b98eb94b41d10b8467b4acbded
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2022-07-19 23:09:58 +0530

    Add files via upload

---
+++ OnlineExamSystem-PHP/photoUpload.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['photoStore'])) {
+    $encoded_data = $_POST['photoStore'];
+    $binary_data = base64_decode($encoded_data);
+
+    $photoname = uniqid().'.jpeg';
+
+    $result = file_put_contents('User_Images/'.$photoname, $binary_data);
+
+    if($result) {
+        echo 'success';
+    } else {
+        echo die('Could not save image! check file permission.');
+    }
+}
+?>
